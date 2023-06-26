1ef54c112c2e3b513ae1afbad96ee250b738b5d4
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-06-26 01:39:39 -0300

    Session SDES/RC4 key + GUI changes

--- client.py
+++ client.py
@@ -21,6 +21,14 @@ class Client:
     def format_nickname(self, nickname):
         ## Replace whitespace for underline and remove newline
         return nickname.replace(" ", "_").strip()
+    
+    def use_sdes(self):
+        self.using_algorithm = "SDES"
+        self.algorithm_label.config(text="SDES")
+
+    def use_rc4(self):
+        self.using_algorithm = "RC4"
+        self.algorithm_label.config(text="RC4")
 
     def use_pkey(self):
         ## User request to use public key
@@ -60,15 +68,11 @@ class Client:
         ## User request to use SDES secret key
         key = self.secret_key_entry.get()
         self.secret_key_SDES = utils.get_SDES_key(key)
-        self.using_algorithm = "SDES"
-        self.algorithm_label.config(text="SDES")
 
     def get_secret_key_RC4(self):
         ## User request to use RC4 secret key
         key = self.secret_key_entry.get()
         self.secret_key_RC4 = key
-        self.using_algorithm = "RC4"
-        self.algorithm_label.config(text="RC4")
 
     def get_session_value(self):
         ## User request to use public value for DH (Diffie-Hellman)
@@ -104,19 +108,19 @@ class Client:
 
         ## Broadcast uses public key and SDES ECB encryption as default
         if (self.sendTo == "Broadcast"):
-            self.chat_label.config(text="BROADCAST")
-            self.key_label.config(text="PUBLIC")
-            self.sdes_op_label.config(text="ECB")
 
             crypt_message = f'{self.nickname}: {ecb_cripto(message.strip(), self.public_key)}'
             self.sock.send(f"BROADCAST{crypt_message}".encode('utf-8'))
+
+            self.chat_label.config(text="BROADCAST")
+            self.key_label.config(text="PUBLIC")
+            self.sdes_op_label.config(text="ECB")
 
         ## DM messages handler
         elif (self.sendTo != "Broadcast"):
 
             ## Client using public key
             if (self.using_key == "Public"):
-
                 key = self.public_key
 
                 if (self.using_algorithm == "SDES"):
@@ -132,12 +136,16 @@ class Client:
 
                 self.sock.send(f"DM {crypt_message} {self.using_algorithm}".encode('utf-8'))
 
-            ## Client using secret key
-            elif (self.using_key == "Secret"):
+            ## Client using secret/session key
+            elif (self.using_key == "Secret" or self.using_key == "Session"):
 
                 if (self.using_algorithm == "SDES"):
+                    
+                    if (self.using_key == "Secret"):
+                        key = self.secret_key_SDES
 
-                    key = self.secret_key_SDES
+                    else:
+                        key = self.session_key_SDES
 
                     if (self.sdes_op == "ECB"):
                         crypt_message = f'{self.nickname} to {self.sendTo}: {ecb_cripto(message.split(), key)}'
@@ -150,7 +158,13 @@ class Client:
                     self.sock.send(f'SKEY {crypt_message} {crypted_key} SDES'.encode('utf-8'))
 
                 elif (self.using_algorithm == "RC4"):
-                    key = self.secret_key_RC4
+
+                    if (self.using_key == "Secret"):
+                        key = self.secret_key_RC4
+
+                    else:
+                        key = self.session_key_RC4
+
                     crypt_message = f'{self.nickname} to {self.sendTo}: {utils.RC4_crypt(message, key)}'
                     crypted_key = utils.RC4_crypt(key, self.public_key)
                     self.sock.send(f'SKEY {crypt_message} {key} RC4'.encode('utf-8'))
@@ -169,8 +183,11 @@ class Client:
             try:
                 message = self.sock.recv(1024).decode('utf-8')
 
-                ## Message from server
+                ## Plaintext Message from server
                 ## print(message)
+
+                ## Flag to show decrypted messages
+                decrypt = False
 
                 ## Empty string after removing client from server
                 if not message:
@@ -197,6 +214,9 @@ class Client:
                         self.sendTo = request_nickname
                         self.chat_label.config(text="DM")
 
+                    ## Don't broadcast this message 
+                    continue
+
                 elif "GET SESSION" in message:
 
                     request_nickname1 = message.split()[2]
@@ -212,10 +232,11 @@ class Client:
 
                 elif "SESSION VALUE" in message:
 
-                    self.request_mod_value = int(message.split()[2])
-                    self.session_key = (self.request_mod_value ** self.session_value) % prime_num
+                    request_mod_value = int(message.split()[2])
+                    self.session_key = (request_mod_value ** self.session_value) % prime_num
 
-                    print(self.session_key)
+                    self.session_key_SDES = cbc_cripto(self.session_key, self.public_key)
+                    self.session_key_RC4 = utils.RC4_crypt(self.session_key, self.public_key)
                     
                     self.using_key = "Session"
                     self.key_label.config(text="SESSION")
@@ -224,15 +245,14 @@ class Client:
                 elif "DECRYPT" in message:
                     
                     ## Encrypted BROADCAST message
+                    decrypt = True
                     decrypt_message = self.decrypt_message_SDES(message, self.public_key, op="ECB")
-                    self.text_area.config(state='normal')
-                    self.text_area.insert('end', decrypt_message)
-                    self.text_area.yview('end')
-                    self.text_area.config(state='disabled')
 
                 elif "PKEY" in message:
 
                     ## Encrypted DM message (Public Key)
+                    decrypt = True
+
                     if "SDES" in message:
                         crypted_message = f"{message.split()[1]} {message.split()[2]} {message.split()[3]} {message.split()[4]}"
 
@@ -248,34 +268,35 @@ class Client:
                         decrypt_message = utils.RC4_decrypt(content, self.public_key)
                         decrypt_message = f"{nicknames} {decrypt_message}"
 
-                    self.text_area.config(state='normal')
-                    self.text_area.insert('end', decrypt_message)
-                    self.text_area.yview('end')
-                    self.text_area.config(state='disabled')
-
-                ## Decrypt messages using Secret Key (DM messages)
+                ## Decrypt messages using Secret/Session Key (DM messages)
                 elif "SKEY" in message:
 
-                    ## Encrypted DM message (Secret Key)
+                    ## Encrypted DM message (Secret/Session Key)
+                    decrypt = True
+
                     if "SDES" in message:
                         crypted_key = message.split()[1]
                         crypted_message = f"{message.split()[2]} {message.split()[3]} {message.split()[4]} {message.split()[5]}"
 
-                        if (self.sdes_op == "ECB"):            
-                            self.secret_key_SDES = ecb_descripto(crypted_key, self.public_key)
-                            decrypt_message = self.decrypt_message_SDES(crypted_message, self.secret_key_SDES, type="DM", op="ECB")
+                        if (self.sdes_op == "ECB"):
+
+                            key = ecb_descripto(crypted_key, self.public_key)
+                            decrypt_message = self.decrypt_message_SDES(crypted_message, key, type="DM", op="ECB")
 
                         elif (self.sdes_op == "CBC"):
-                            self.secret_key_SDES = cbc_descripto(crypted_key, self.public_key)
-                            decrypt_message = self.decrypt_message_SDES(crypted_message, self.secret_key_SDES, type="DM", op="CBC")
+                            key = cbc_descripto(crypted_key, self.public_key)
+                            decrypt_message = self.decrypt_message_SDES(crypted_message, key, type="DM", op="CBC")
 
                     elif "RC4" in message:
-                        self.secret_key_RC4 = message.split()[1]
+                        key = message.split()[1]
                         nicknames = f"{message.split()[2]} {message.split()[3]} {message.split()[4]}"
                         crypted_message = message.split()[5]
-                        decrypt_message = utils.RC4_decrypt(crypted_message, self.secret_key_RC4)
+    
+                        decrypt_message = utils.RC4_decrypt(crypted_message, key)
                         decrypt_message = f"{nicknames} {decrypt_message}"
 
+                ## Decrypt messages
+                if (decrypt):
                     self.text_area.config(state='normal')
                     self.text_area.insert('end', decrypt_message)
                     self.text_area.yview('end')
@@ -328,7 +349,7 @@ class Client:
         self.send_button.config(font=("Arial", 12))
         self.send_button.pack(padx=20, pady=5)
 
-        self.send_to_label = tkinter.Label(self.window, text="Send DM (nickname or IP): ", bg="lightgray")
+        self.send_to_label = tkinter.Label(self.window, text="Send DM (name/IP): ", bg="lightgray")
         self.send_to_label.config(font=("Arial", 12))
         self.send_to_label.pack(padx=10, pady=10, side=tkinter.LEFT)
 
@@ -342,17 +363,17 @@ class Client:
         self.secret_key_label = tkinter.Label(self.window, text="Secret Key: ", bg="lightgray")
         self.secret_key_label.config(font=("Arial", 12))
         self.secret_key_label.pack(padx=10, pady=10, side=tkinter.LEFT)
-        
-        self.secret_key_button_SDES = tkinter.Button(self.window, text="SDES", command=self.get_secret_key_SDES)
-        self.secret_key_button_SDES.config(font=("Arial", 12))
-        self.secret_key_button_SDES.pack(padx=10, pady=10, side=tkinter.LEFT)
-
-        self.secret_key_button_RC4 = tkinter.Button(self.window, text="RC4", command=self.get_secret_key_RC4)
-        self.secret_key_button_RC4.config(font=("Arial", 12))
-        self.secret_key_button_RC4.pack(padx=10, pady=10, side=tkinter.LEFT)
 
         self.secret_key_entry = tkinter.Entry(self.window)
         self.secret_key_entry.pack(padx=10, pady=10, side=tkinter.LEFT)
+
+        self.secret_key_button_SDES = tkinter.Button(self.window, text="SDES key", command=self.get_secret_key_SDES)
+        self.secret_key_button_SDES.config(font=("Arial", 12))
+        self.secret_key_button_SDES.pack(padx=10, pady=10, side=tkinter.LEFT)
+
+        self.secret_key_button_RC4 = tkinter.Button(self.window, text="RC4 key", command=self.get_secret_key_RC4)
+        self.secret_key_button_RC4.config(font=("Arial", 12))
+        self.secret_key_button_RC4.pack(padx=10, pady=10, side=tkinter.LEFT)
 
         self.session_value_label = tkinter.Label(self.window, text="Session Key Value: ", bg="lightgray")
         self.session_value_label.config(font=("Arial", 12))
@@ -364,6 +385,18 @@ class Client:
         self.session_value_button = tkinter.Button(self.window, text="Send Value", command=self.get_session_value)
         self.session_value_button.config(font=("Arial", 12))
         self.session_value_button.pack(padx=10, pady=10, side=tkinter.LEFT)
+
+        self.algorithm_label = tkinter.Label(self.window, text="Select Algorithm: ", bg="lightgray")
+        self.algorithm_label.config(font=("Arial", 16))
+        self.algorithm_label.pack(padx=20, pady=5, side=tkinter.TOP)
+        
+        self.use_sdes_button = tkinter.Button(self.window, text="SDES", command=self.use_sdes)
+        self.use_sdes_button.config(font=("Arial", 12))
+        self.use_sdes_button.pack(padx=20, pady=5)
+
+        self.use_rc4_button = tkinter.Button(self.window, text="RC4", command=self.use_rc4)
+        self.use_rc4_button.config(font=("Arial", 12))
+        self.use_rc4_button.pack(padx=20, pady=5)
 
         self.sdes_op_label = tkinter.Label(self.window, text="Select SDES op: ", bg="lightgray")
         self.sdes_op_label.config(font=("Arial", 16))
@@ -420,10 +453,13 @@ class Client:
 
         self.HOST_IP = simpledialog.askstring("IP", "IP:", parent=IP_window)
 
+        ## Check if given IP is valid
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.connect((self.HOST_IP, PORT))
         except:
+            
+            ## Invalid IP or server is not running
             print(f"Connection refused to the server.")
             print(f"Requested IP: {self.HOST_IP} | Requested PORT: {PORT}")
             exit(-1)
@@ -438,20 +474,23 @@ class Client:
         self.gui_done = False
         self.running = True
 
+        ## Default settings
         self.using_key = "Public"
         self.sendTo = "Broadcast"
         self.using_algorithm = "SDES"
         self.sdes_op = "ECB"
+
+        ## User settings
         self.secret_key_SDES = ""
         self.secret_key_RC4 = ""
-        self.session_value = ""
         self.client_mod_value = ""
-        self.request_mod_value = ""
+        self.session_value = ""
         self.session_key = ""
+        self.session_key_SDES = ""
+        self.session_key_RC4 = ""
 
         gui_thread = threading.Thread(target=self.gui_loop)
         receive_thread = threading.Thread(target=self.receive)
-
         gui_thread.start()
         receive_thread.start()
 
